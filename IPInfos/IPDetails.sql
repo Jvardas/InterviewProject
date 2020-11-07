32c5dc12bd0a31e0c681efc2cf4610c5d99ec596
Jvardas <jvardas7@gmail.com>
2020-11-07 17:43:20 +0200

    Add database project with IPDetails table

---
+++ IPInfos/IPDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[IPDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [City] NVARCHAR(100) NULL, 
+    [Country] NVARCHAR(100) NULL, 
+    [Continent] NVARCHAR(100) NULL, 
+    [Latitude] DECIMAL(12, 9) NULL, 
+    [Longitude] DECIMAL(12, 9) NULL
+)
