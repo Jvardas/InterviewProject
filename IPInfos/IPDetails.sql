c0b060546c4220f62969fa8d7db5336497012d70
Jvardas <jvardas7@gmail.com>
2020-11-08 22:26:12 +0200

    Remove unwanted projects

--- IPInfos/IPDetails.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[IPDetails]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [City] NVARCHAR(100) NULL, 
-    [Country] NVARCHAR(100) NULL, 
-    [Continent] NVARCHAR(100) NULL, 
-    [Latitude] DECIMAL(12, 9) NULL, 
-    [Longitude] DECIMAL(12, 9) NULL
-)
